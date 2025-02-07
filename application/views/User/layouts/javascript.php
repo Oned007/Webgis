d1d65b19ed45ce189c1f40f7a2fbff65d47110c4
Oned007 <daniwahyudi117@gmail.com>
2025-02-07 15:38:23 +0700

    Little Change

--- application/views/User/layouts/javascript.php
+++ application/views/User/layouts/javascript.php
@@ -13,35 +13,22 @@
 <script src="<?=templates('vendors/nprogress/nprogress.js')?>"></script>
 
 <!-- JS Vendors -->
-<script src="<?=base_url('assets/js/vendor/js/vendors.min.js')?>"></script>
-<script src="<?=base_url('assets/js/vendor/js/forms/select/select2.full.min.js')?>"></script>
+<script src="<?=base_url('assets/js/vendors/js/forms/select/select2.full.min.js')?>"></script>
 
-<script src="<?=base_url('assets/js/vendor/js/ui/prism.min.js')?>"></script>
-<script src="<?=base_url('assets/js/vendor/js/tables/datatable/datatables.min.js')?>"></script>
-<script src="<?=base_url('assets/js/vendor/js/tables/datatable/datatables.bootstrap4.min.js')?>"></script>
-<script src="<?=base_url('assets/js/vendor/js/charts/apexcharts.min.js')?>"></script>
-<script src="<?=base_url('assets/js/vendor/js/pickers/pickadate/picker.js')?>"></script>
-<script src="<?=base_url('assets/js/vendor/js/pickers/pickadate/picker.date.js')?>"></script>
+<script src="<?=base_url('assets/js/vendors/js/ui/prism.min.js')?>"></script>
+
 
 <!-- Theme JS -->
 <script src="<?=base_url('assets/js/core/app-menu.js')?>"></script>
 <script src="<?=base_url('assets/js/core/app.js')?>"></script>
 
 <!-- Page JS -->
-<script src="<?=base_url('assets/js/scripts/datatables/datatable.js')?>"></script>
-<script src="<?=base_url('assets/js/scripts/pages/user-profile.js')?>"></script>
-<script src="<?=base_url('assets/js/scripts/pickers/dateTime/pick-a-datetime.js')?>"></script>
-<script src=" <?=base_url('assets/js/scripts/extensions/ext-component-ratings.js')?>"></script>
-<script src=" <?=base_url('assets/js/vendor/js/extensions/jquery.rateyo.min.js')?>"></script>
 <script src="<?=base_url('assets/js/scripts/forms/select/form-select2.js')?>"></script>
 
 <!-- Custom Theme Scripts -->
 <script src="<?=templates('build/js/custom.min.js')?>"></script>
 <script type="text/javascript" src="https://cdn.datatables.net/v/bs4/dt-1.10.22/datatables.min.js"></script>
 
-    <!-- BEGIN: Vendor JS-->
-    <script src="<?=base_url('assets/vendor/js/vendors.min.js')?>"></script>
-    <!-- BEGIN Vendor JS-->
 
     <!-- BEGIN: Page Vendor JS-->
     <script src="<?=base_url('assets/vendor/js/charts/apexcharts.min.js')?>"></script>
@@ -58,7 +45,5 @@
     <!-- BEGIN: Page JS-->
     <script src="<?=base_url('assets/js/scripts/pages/dashboard-ecommerce.js')?>"></script>
     <script src="<?=base_url('assets/js/scripts/extensions/swiper.js')?>"></script>
-    <script src="<?=base_url('assets/ctrl/front/js/bootstrap-datepicker.js')?>"></script>
-    <script src="<?=base_url('assets/ctrl/front/js/jquery.timepicker.min.js')?>"></script>
     <script src="<?=base_url('assets/ctrl/front/js/custom.js')?>"></script>
     <script src="<?=base_url('assets/js/scripts/forms/select/form-select2.js')?>"></script>
